f1ef175abceeb1b3b076b3fa75d71c92224b1559
jiang liang jun <675132271@qq.com>
2015-04-22 21:47:57 +0800

    修改logo

--- gt-android-sdk-demo/src/com/gt/demo/MainActivity.java
+++ gt-android-sdk-demo/src/com/gt/demo/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
@@ -30,6 +31,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 
 		findViewById(R.id.btn_gtapp_sdk_demo_dlg).setOnClickListener(
